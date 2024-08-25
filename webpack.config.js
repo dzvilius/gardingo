0244de53111b6304f0eca9dc6ad522515aa98772
Vilius Dzemyda <dev@viliusdzemyda.com>
2024-08-25 20:41:17 +0100

    add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
+      favicon: path.join(__dirname, 'public', 'favicon.ico'),
     }),
     new MiniCssExtractPlugin({
       filename: 'styles.css',
